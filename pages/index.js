3d5f3244986378eb45118c7c3a25aa894213e7f9
chazzsplash <64711302+chazz-codes@users.noreply.github.com>
2022-04-03 18:01:57 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -24,10 +24,6 @@ export default function Home({ allPostsData }) {
         <p>Hey I'm <b>Chazz</b>. I'm a software engineer and I enjoy lofi music.
         contact me on <a href='http://twitter.com/chazzsplash'>twitter</a>
         </p>
-        <p>
-          (This is a sample website - you'll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
