73d9f339e4700461d867daacf8c30a9b65f38e22
chazzsplash <64711302+chazz-codes@users.noreply.github.com>
2022-04-03 18:00:00 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home({ allPostsData }) {
         contact me on <a href='http://twitter.com/chazzsplash'>twitter</a>
         </p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          (This is a sample website - you'll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
