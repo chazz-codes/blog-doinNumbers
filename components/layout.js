8230ac388794fb9f944e8af1a5cf36457297188b
chazzsplash <64711302+chazz-codes@users.noreply.github.com>
2022-04-08 14:53:29 -0400

    preview image

--- components/layout.js
+++ components/layout.js
@@ -4,6 +4,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
+
 const name = 'Chazz'
 export const siteTitle = 'doinNumbers'
 
@@ -18,9 +19,7 @@ export default function Layout({ children, home }) {
         />
         <meta
           property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content="/preview-img.png"
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
