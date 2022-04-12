78a89c0f102bf380cc12193efef9ca8f20ab7013
chazzsplash <64711302+chazz-codes@users.noreply.github.com>
2022-04-12 18:35:54 -0400

    Revert "style change"
    This reverts commit 94a106a9f179883b0bee6f837563e24e25d9dce5.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
@@ -18,23 +19,23 @@ export default function Home({ allPostsData }) {
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
-
+        
       </Head>
-      <section>
+      <section className={utilStyles.headingMd}>
         <p>Hey my name is <b>Chazz</b>. I am a software engineer and I enjoy lofi music.
         contact me via <a href='mailto:chazz@doinnumbers.com'>email</a>
         </p>
       </section>
-      <section>
-        <h2>Blog</h2>
-        <ul>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
-            <li key={id}>
+            <li className={utilStyles.listItem} key={id}>
             <Link href={`/posts/${id}`}>
               <a>{title}</a>
             </Link>
             <br />
-            <small >
+            <small className={utilStyles.lightText}>
               <Date dateString={date} />
             </small>
           </li>
