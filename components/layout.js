4dd4a634962d783d2a21c8edd38fe745576d8d6c
chazzsplash <64711302+chazz-codes@users.noreply.github.com>
2022-04-12 18:36:05 -0400

    Revert "Revert "style change""
    This reverts commit 78a89c0f102bf380cc12193efef9ca8f20ab7013.

--- components/layout.js
+++ components/layout.js
@@ -1,19 +1,21 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 import previewImage from '../public/images/preview-img.png'
-
+import Header from './Header'
 
 const name = 'Chazz'
 export const siteTitle = 'doinNumbers'
 
 export default function Layout({ children, home }) {
   return (
-    <div className={styles.container}>
+    <div className="container mx-auto px-10 mb-8">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+      <link rel="manifest" href="/site.webmanifest"/>
         <meta
           name="description"
           content={previewImage}
@@ -25,41 +27,7 @@ export default function Layout({ children, home }) {
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
-      <header className={styles.header}>
-        {home ? (
-          <>
-            <Image
-              priority
-              src="/images/profile.jpg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt={name}
-            />
-            <h1 className={utilStyles.heading2Xl}>{siteTitle}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <a>
-                <Image
-                  priority
-                  src="/images/profile.jpg"
-                  className={utilStyles.borderCircle}
-                  height={108}
-                  width={108}
-                  alt={name}
-                />
-              </a>
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/">
-                <a className={utilStyles.colorInherit}>{name}</a>
-              </Link>
-            </h2>
-          </>
-        )}
-      </header>
+      <Header />
       <main>{children}</main>
       {!home && (
         <div className={styles.backToHome}>
