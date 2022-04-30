8046aeb59f3d1eb6b7493d08125656b291a80d32
chazzsplash <64711302+chazz-codes@users.noreply.github.com>
2022-04-29 20:35:32 -0400

    quotesAPI and graphcms connected
    graphcms api connected, further functionality to be added later

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,72 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import { getSortedPostsData } from '../lib/posts'
+import { fetchData } from '../lib/quotes'
 import Link from 'next/link'
 import Date from '../components/date'
+import axios from 'axios';
+import { useEffect, useState } from 'react'
+import { getGraphCMS } from '../lib/graphcms'
 
 export async function getStaticProps(){
   const allPostsData = getSortedPostsData();
+  const quotesData = await fetchData()
+  const graphCMSPosts = await (await getGraphCMS()).props.posts;
+  console.log(graphCMSPosts)
+  
+
   return {
     props: {
-      allPostsData
+      allPostsData, 
+      quotesData,
+      graphCMSPosts
     }
   }
 }
 
-export default function Home({ allPostsData }) {
+export default function Home({ allPostsData, quotesData, graphCMSPosts }) {
+ 
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
 
       </Head>
-      <section>
-        <p>Hey my name is <b>Chazz</b>. I am a software engineer and I enjoy lofi music.
-        contact me via <a href='mailto:chazz@doinnumbers.com'>email</a>
-        </p>
-      </section>
-      <section className='Blog-Main'>
-        <h2>Blog</h2>
-        <ul>
-          {allPostsData.map(({ id, date, title }) => (
-            <li key={id}>
-            <Link href={`/posts/${id}`}>
-              <a>{title}</a>
-            </Link>
-            <br />
-            <small >
-              <Date dateString={date} />
-            </small>
-          </li>
-          ))}
-        </ul>
-      </section>
+      <div className="homeBody">
+        <div className="qotd">
+          <h2>Quote of the Day</h2>
+          <div dangerouslySetInnerHTML={{__html: quotesData[0].h}}/>
+        </div>
+        <div className="blogFeed">
+          <section>
+            <p>Hey my name is <b>Chazz</b>. I am a software engineer and I enjoy lofi music.
+            contact me via <a href='mailto:chazz@doinnumbers.com'>email</a>
+            </p>
+            
+          </section>
+          <section className='Blog-Main'>
+            <h2>Blog</h2>
+            <ul>
+              {allPostsData.map(({ id, date, title }) => (
+                <li key={id}>
+                <Link href={`/posts/${id}`}>
+                  <a>{title}</a>
+                </Link>
+                <br />
+                <small >
+                  <Date dateString={date} />
+                </small>
+              </li>
+              ))}
+            </ul>
+          </section>
+          {/* <div className="graphCMSPosts">
+              {graphCMSPosts.map(({id, title, featuredImage}) => (
+                <li key={id}> {title} </li>
+              ))}
+          </div> */}
+        </div>
+      </div>
     </Layout>
   )
 }
