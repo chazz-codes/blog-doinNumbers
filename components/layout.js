fc513bd747c752da8928efc640d65223b52f0168
chazzsplash <64711302+chazz-codes@users.noreply.github.com>
2022-05-13 15:49:13 -0400

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -27,7 +27,7 @@ export default function Layout({ children, home }) {
         />
         <meta
           property="og:image"
-          content={previewImage.src}
+          content={`"${previewImage.src}"`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
