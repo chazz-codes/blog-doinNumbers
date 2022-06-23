2720f7cd087acf5c39afc428e11d8c627b7f7d21
chazzsplash <64711302+chazz-codes@users.noreply.github.com>
2022-06-23 13:19:49 -0400

    Update QuoteofTheDay.jsx

--- components/QuoteofTheDay.jsx
+++ components/QuoteofTheDay.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import useSWR from 'swr'
 
 const QuoteofTheDay = () => {
     const fetcher = (...args) => fetch(...args).then((res) => res.json())
-    const { data, error } = useSWR('https://zenquotes.io/api/today', fetcher)
+    // const { data, error } = useSWR('https://zenquotes.io/api/today', fetcher)
+    const {isLoading, setIsLoading} = useState(true)
+    const {quoteData, setQuoteData} = useState(null)
+
+
+    useEffect( () => {
+      fetch("https://zenquotes.io/api/today")
+    .then( (res) => res.json())
+    .then((data) => {
+      setQuote(data)
+      setIsLoading(false)
+    })
+}, [])
+
 
   return (
     <React.Fragment>
         <h2>Quote of the Day </h2>
-           
+           {isLoading ? <h2>Now Loading...</h2> :
               <div dangerouslySetInnerHTML={{__html: data[0].h}}/> 
+  }
     </React.Fragment>
   )
 }
